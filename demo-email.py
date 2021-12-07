34b4ae91c668b681c00610deaa0da1f25770a383
Alvaro Lara <alvaro1542@gmail.com>
2021-12-07 16:50:39 -0300

    Enviador básico de correos electronicos
    ARREGLOS SIMPLES

--- demo-email.py
+++ demo-email.py
@@ -6,6 +6,7 @@ GOOGLE_KEY = os.environ.get('GOOGLE_KEY')
 EMAIL_RECIEVER = os.environ.get('EMAIL_OTHER')
 
 with smtplib.SMTP('smtp.gmail.com', 587) as smtp: # Conexion
+  
   smtp.ehlo()  # nos identifica con el servidor principal
   smtp.starttls()  # Encriptar trafico
   smtp.ehlo()  # Reidentificar
